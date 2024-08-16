88b8df7a12018747f76ad668ea476132408ef63a
Eugene Heynike <eugeneheynike@nexablend.co.za>
2024-08-16 18:49:45 +0200

    changed website name

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="The official home of Cantime Productions and Inked Up Industries"
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="Cantime Productions" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="The official home of Cantime Productions and Inked Up Industries"
           />
           <meta property="og:title" content="Next.js Conf 2022 Pictures" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="The official home of Cantime Productions and Inked Up Industries"
           />
         </Head>
         <body className="bg-black antialiased">
